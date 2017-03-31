50b8b0318e0fcf38fdf9cf0240538f3a4aa2c2d1
nikhilnbv <nikhilnbv@gmail.com>
2017-03-31 15:12:40 +0530

    Update app.admin.component.ts

--- src/app/app.admin.component.ts
+++ src/app/app.admin.component.ts
@@ -6,135 +6,11 @@ import {ElementRef,Renderer2} from '@angular/core';
 
 import { OnInit } from '@angular/core';
 
-//import './scripts/bootstrap-3.3.7.min.js';
-//import './scripts/jquery-3.2.0.min.js';
-
-
-
 @Component({
     selector:'app-admin',
-    template:`
-                <!doctype html>
-                <html>
-                    <head>
-                        <style>
-                            .app-header{
-                                width:99.9%;
-                                height:50px;
-                                background-color: rgb(45, 62, 80);
-                                border: 1px;
-                                border-style: solid;
-                                border-color: lightgrey;
-                            }
-                            .header-welcome-text{
-                                float: left;
-                                padding: 1%;
-                                color: white;
-                                font-weight: bold;
-                                font-family: sans-serif;
-                            }
-                            .header-menu{
-                                float: right;
-                            }
-                            .app-hero{
-                                width:99.9%;
-                                height:100px;
-                                background-color:rgb(220, 233, 233);
-                                border: 1px;
-                                border-style: solid;
-                                border-color: lightgrey;
-                                border-bottom: 0px;
-                            }
-                            .hero-logo{
-                                float: left;
-                                width:25%;
-                            }
-                            .hero-text{
-                                width:65%;
-                                text-align: right;
-                                float: left;
-                                margin: 25px;
-                                text-align: center;
-                                font-family: sans-serif;
-                                font-weight:bold;
-                                font-size: 35px;
-                                color: rgb(0, 149, 200);
-                            }
-                            .nav-links{
-                                width:24.6%;
-                                float: left;
-                                height: 200px;
-                                background-color:rgb(220, 233, 180);
-                                border: 1px;
-                                border-style: solid;
-                                border-color: lightgrey;
-                            }
-                            .nav-content{
-                                width:75.1%;
-                                float: left;
-                                height: 200px;
-                                background-color:rgb(220, 233, 233);
-                                border: 1px;
-                                border-left: 0px;
-                                border-style: solid;
-                                border-color: lightgrey;
-                            }
-                        </style>
-                    </head>
-                    <body>
-
-                        <section class="app-hero">
-                            <img class="hero-logo" src="src/app/img/Logo_Cybage_5.png" />
-                            <span class="hero-text">Training Management System</span>
-                        </section>
-
-                        <header class="app-header">
-                            <div class="header-welcome-text">Welcome Admin</div>
-                            <div class="header-menu">
-                                <div class="dropdown">
-                                    <a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#">
-                                        Dropdown
-                                        <b class="caret"></b>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a>Logout</a></li>
-                                    </ul>
-                                </div>
-                                <button type="submit" (click)="logout()" >Logout</button>
-                            </div>
-                        </header>
-
-                        <nav>
-                            <div class="nav-links">
-                                <span class="span-links"><a (click)="manageUserScreen()">Manage User</a></span><br>
-                                <span class="span-links"><a (click)="manageUserScreen()">Manage Training</a></span>
-                            </div>
-                            <div class="nav-content">
-                                <div id="main-content"></div>
-                            </div>
-                        </nav>
-                        
-
-                        <script type="text/javscript">
-
-                            <!-- for desabling back button -->
-                            function preventBack()
-                            {
-                                window.history.forward();
-                            }
-                            setTimeout("preventBack()",0);
-                            window.onunload=function() {null};                           
-                        </script>
-
-
-                    </body>
-                </html>
-
-
-
-    `,
+    templateUrl:'./html/template.html',
     providers:[LoginService],
-    styleUrls: ['styles/bootstrap-theme.min.css','styles/bootstrap-3.3.7.css']
+    styleUrls:['./styles/template.css']
 })
 
 export class AdminComponent implements OnInit{
@@ -150,7 +26,7 @@ export class AdminComponent implements OnInit{
                 '<div class="panel panel-default">'+
                 '<div class="panel-heading">'+
                     '<h4 class="panel-title">'+
-                    '<a data-toggle="collapse" href="#collapse1">Collapsible panel</a>'+
+                    '<a data-toggle="collapse" data-target="#collapse1">User Registration</a>'+
                     '</h4>'+
                 '</div>'+
                 '<div id="collapse1" class="panel-collapse collapse">'+
@@ -159,12 +35,42 @@ export class AdminComponent implements OnInit{
                 '</div>'+
                 '</div>'+
             '</div>'+
+        '</div>'+
+
+        '<div class="container" style="width:100%;">'+
+            '<div class="panel-group">'+
+                '<div class="panel panel-default">'+
+                '<div class="panel-heading">'+
+                    '<h4 class="panel-title">'+
+                    '<a data-toggle="collapse" href="#collapse2">User Details</a>'+
+                    '</h4>'+
+                '</div>'+
+                '<div id="collapse2" class="panel-collapse collapse">'+
+                    '<div class="panel-body">Panel Body</div>'+
+                    '<div class="panel-footer">Panel Footer</div>'+
+                '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+
+        '<div class="container">'+
+            '<button type="button" class="btn btn-info" (click)="callMe()" data-toggle="collapse" data-target="#demo345">Simple collapsible</button>'+
+            '<div id="demo345" class="collapse">'+
+                'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'+
+                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,'+
+                'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'+
+            '</div>'+
         '</div>';
+
     }
 
     ngOnInit(){
         console.log('onInit event called..');
-        this._elementRef.nativeElement.querySelector('.dropdown-toggle').dropdown();
+        //this._elementRef.nativeElement.querySelector('.dropdown-toggle').dropdown();
+    }
+
+    callMe(){
+        console.log('btn clicked');
     }
 
     logout() { 
