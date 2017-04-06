edce3e8446c6e09597bdd82105a69dba9dca6d78
nikhilnbv <nikhilnbv@gmail.com>
2017-04-06 12:59:57 +0530

    Update app.router.ts

--- src/app/services/app.router.ts
+++ src/app/services/app.router.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from '../admin/app.admin.component';
+import { TrainerComponent } from '../trainer/app.trainer.component';
+import { TraineeComponent } from '../trainee/app.trainee.component';
 import { LoginComponent } from '../app.login.component';
 import { AuthGuard } from '../guard/authGuard';
 
@@ -8,6 +10,8 @@ import { AuthGuard } from '../guard/authGuard';
 export const routes: Routes = [
 
     { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },    
+    { path: 'trainer', component: TrainerComponent, canActivate: [AuthGuard] },    
+    { path: 'trainee', component: TraineeComponent, canActivate: [AuthGuard] },    
     { path: 'login', component: LoginComponent },    
     { path: '', component: LoginComponent },    
     // otherwise redirect to home
