87aae6a14780ef682635fe68632675e02c201625
nikhilnbv <nikhilnbv@gmail.com>
2017-04-06 13:00:46 +0530

    Update loginService.ts

--- src/app/services/loginService.ts
+++ src/app/services/loginService.ts
@@ -4,30 +4,29 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class LoginService {
+    
     constructor() { }
 
         login = function (username: string, password: string) : boolean {
             if (username == "admin" && password == "123456") {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', username);               
+                localStorage.setItem('userRole', 'admin');               
+                return true;
+            }
+            else if (username == "trainer" && password == "123456") {
                 localStorage.setItem('currentUser', username);
-                //localStorage.setItem('password', password);
+                localStorage.setItem('userRole', 'trainer');               
+                return true;
+            }
+            else if (username == "trainee" && password == "123456") {
+                localStorage.setItem('currentUser', username);
+                localStorage.setItem('userRole', 'trainee');               
                 return true;
             }
             return false;
-
-        // return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
-        //     .map((response: Response) => {
-        //         // login successful if there's a jwt token in the response
-        //         let user = response.json();
-        //         if (user && user.token) {
-        //             // store user details and jwt token in local storage to keep user logged in between page refreshes
-        //             localStorage.setItem('currentUser', JSON.stringify(user));
-        //         }
-        //     });
     }
 
     logout() {
-        // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
     }
 }
