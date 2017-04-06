088c5883859e4dbb3c1fb9d2be35ea4bb21c147d
nikhilnbv <nikhilnbv@gmail.com>
2017-04-06 13:02:17 +0530

    Update app.trainee.component.ts

--- src/app/trainee/app.trainee.component.ts
+++ src/app/trainee/app.trainee.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { LoginService } from '../services/loginService';
+import { Router } from '@angular/router';
+import { routes } from '../services/app.router';
 
 @Component({
     selector:'app-trainee',
-    template:'<h1>Welcome Trainee</h1>'
+    templateUrl:'../html/template.html',
+    styleUrls:['../styles/template.css']
 })
 
-export class TraineeComponent {
+export class TraineeComponent implements OnInit {
 
+    constructor(private loginService : LoginService,private router: Router,private _elementRef : ElementRef) {
+    }
+
+    ngOnInit(){
+        if(localStorage.getItem('userRole').toString() == "trainee"){ 
+            //this.renderer.setElementProperty(this._elementRef.nativeElement.querySelector('#adminSpan1'), 'hidden', false);                            
+            this._elementRef.nativeElement.querySelector('#adminLinks').hidden = true;
+            this._elementRef.nativeElement.querySelector('#trainerLinks').hidden = true;
+            this._elementRef.nativeElement.querySelector('#traineeLinks').hidden = false;
+        }            
+    }
+
+    logout() { 
+        //console.log('calling logout');
+        this.loginService.logout();
+        this.router.navigate(['/login']);
+    }
 }
